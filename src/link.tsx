65f3717d40aeab983ff0d6c380189a5a67fd04a5
Erick Gustavo <erick.stazs@gmail.com>
2022-12-04 22:57:47 -0300

    First commit

---
+++ src/link.tsx
@@ -0,0 +1,17 @@
+type LinkProps = {
+  title: string
+}
+
+function Link({ title }: LinkProps) {
+  return (
+    <a href="#" className='ease-linear duration-100 font-semibold text-white
+    hover:text-emerald-400 uppercase py-2 px-8 backdrop-blur-sm cursor-pointer
+    hover:bg-white/30 rounded-md inline-block'
+    >
+      {title}
+    </a>
+  )
+}
+
+export { Link }
+
