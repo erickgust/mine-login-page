27fc5c18eaccee72ffb9571cfcf5cf9509ed4516
Erick Gustavo <erick.stazs@gmail.com>
2022-12-27 00:36:50 -0300

    Add header for mobile

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,7 @@ function App () {
       <div className='bg-mine-background bg-no-repeat bg-cover h-full w-full fixed -z-10' />
       <div className='max-sm:backdrop-brightness-50 h-full w-full fixed z-0' />
       <main
-        className='flex flex-col justify-between items-center py-10 z-10 relative'
+        className='flex flex-col justify-between items-center py-10 z-10 px-6 relative'
       >
         <Header />
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,27 @@
 import { Link } from './link'
 import logo from '../assets/logo.png'
+import { ReactComponent as Menu } from '../assets/menu.svg'
 
 function Header () {
   return (
-    <header className='flex flex-col justify-center items-center'>
-      <div className='mb-8'>
-        <img src={logo} alt='Logo Mine' />
+    <header className='flex w-full items-center justify-between sm:flex-col sm:justify-center'>
+      <div className='sm:mb-8'>
+        <img src={logo} alt='Logo Mine'/>
       </div>
 
-      <ul className='flex md:gap-2 gap-1'>
+      <ul className='hidden sm:flex md:gap-2'>
         <li><Link title='Entrar' /></li>
         <li><Link title='Forum' /></li>
         <li><Link title='Sobre' /></li>
         <li><Link title='Mojang' /></li>
         <li><Link title='Perguntas' /></li>
       </ul>
+
+      <div className='flex sm:hidden'>
+        <button type='button' aria-label='toggle menu'>
+          <Menu />
+        </button>
+      </div>
     </header>
   )
 }
